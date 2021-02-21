900f7964d3a759662ff76a780d65710aab86a71c
dillipnayak <dillipnayak@dillips-mbp.hitronhub.home>
2021-02-21 16:45:53 -0500

    Added selenium util methods

--- src/main/java/test/Submit.java
+++ src/main/java/test/Submit.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Iterator;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.log4j.Logger;
@@ -25,30 +27,11 @@ import org.testng.annotations.Test;
 
 
 public class Submit {
-	
+
 	Logger logger = Logger.getLogger(Submit.class.getName()); 
-	WebElement signUpLink= null;
-	WebElement selUserType= null;
-	WebElement selLocation= null;
-	WebElement txtName= null;
-	WebElement txtEmpNo= null;
-	WebElement radioGenderMale= null;
-	WebElement radioGenderFemale= null;
-	WebElement txtEmail= null;
-	WebElement btnCreateUser= null;
-	WebElement lableSuccess= null;
-	
 
-	WebElement loginLink= null;
-	WebElement txtUsername= null;
-	WebElement txtPassword= null;
 
-	WebElement btnAddChem= null;
-	WebElement btnViewChem= null;
-	WebElement txtAddName= null;
-	WebElement txtAddSrNo= null;
-	WebElement txtAddType= null;
-	WebElement txtAddQuantity= null;
+
 	public static WebDriver driver =null;
 
 	//initialize driver
@@ -56,43 +39,28 @@ public class Submit {
 	public void initializeDriver() {
 		System.setProperty("webdriver.chrome.driver","/Users/dillipnayak/Downloads/chromedriver");
 		driver = new ChromeDriver();
+
+
+	}
+
+	@Test 
+	public void test01() throws IOException {
+		System.out.println("Running test 01");
 		driver.get("https://www.google.ca/");
 		driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
 		logger.info("Title of the page <<>"+driver.getTitle()+">");
 		logger.info("No of Hyperlinks <<"+driver.findElements(By.tagName("//a")).size()+">>");
-		createLocators();
-	}
-	
-	//Copy Actual & relevant locators from the application under test
-	public void createLocators() {
-		 signUpLink= ce("/html/body/div[1]/div[3]/form/div[2]/div[1]/div[1]/div/div[2]/input");
-		 selUserType= ce("/html/body/div[1]/div[3]/form/div[2]/div[1]/div[1]/div/div[2]/input");
-		 selLocation= ce("/html/body/div[1]/div[3]/form/div[2]/div[1]/div[1]/div/div[2]/input");
-		 txtName= ce("/html/body/div[1]/div[3]/form/div[2]/div[1]/div[1]/div/div[2]/input");
-		 txtEmpNo= ce("/html/body/div[1]/div[3]/form/div[2]/div[1]/div[1]/div/div[2]/input");
-		 radioGenderMale= ce("/html/body/div[1]/div[3]/form/div[2]/div[1]/div[1]/div/div[2]/input");
-		 radioGenderFemale= ce("/html/body/div[1]/div[3]/form/div[2]/div[1]/div[1]/div/div[2]/input");
-		 txtEmail= ce("/html/body/div[1]/div[3]/form/div[2]/div[1]/div[1]/div/div[2]/input");
-		 btnCreateUser= ce("/html/body/div[1]/div[3]/form/div[2]/div[1]/div[1]/div/div[2]/input");
-		 lableSuccess= ce("/html/body/div[1]/div[3]/form/div[2]/div[1]/div[1]/div/div[2]/input");
-		
 
-		 loginLink= ce("/html/body/div[1]/div[3]/form/div[2]/div[1]/div[1]/div/div[2]/input");
-		 txtUsername= ce("/html/body/div[1]/div[3]/form/div[2]/div[1]/div[1]/div/div[2]/input");
-		 txtPassword= ce("/html/body/div[1]/div[3]/form/div[2]/div[1]/div[1]/div/div[2]/input");
+		WebElement signUpLink= ce("/html/body/div[1]/div[3]/form/div[2]/div[1]/div[1]/div/div[2]/input");
+		WebElement selLocation= ce("/html/body/div[1]/div[3]/form/div[2]/div[1]/div[1]/div/div[2]/input");
+		WebElement txtName= ce("/html/body/div[1]/div[3]/form/div[2]/div[1]/div[1]/div/div[2]/input");
+		WebElement txtEmpNo= ce("/html/body/div[1]/div[3]/form/div[2]/div[1]/div[1]/div/div[2]/input");
+		WebElement radioGenderMale= ce("/html/body/div[1]/div[3]/form/div[2]/div[1]/div[1]/div/div[2]/input");
+		WebElement radioGenderFemale= ce("/html/body/div[1]/div[3]/form/div[2]/div[1]/div[1]/div/div[2]/input");
+		WebElement txtEmail= ce("/html/body/div[1]/div[3]/form/div[2]/div[1]/div[1]/div/div[2]/input");
+		WebElement btnCreateUser= ce("/html/body/div[1]/div[3]/form/div[2]/div[1]/div[1]/div/div[2]/input");
+		WebElement lableSuccess= ce("/html/body/div[1]/div[3]/form/div[2]/div[1]/div[1]/div/div[2]/input");
 
-		 btnAddChem= ce("/html/body/div[1]/div[3]/form/div[2]/div[1]/div[1]/div/div[2]/input");
-		 btnViewChem= ce("/html/body/div[1]/div[3]/form/div[2]/div[1]/div[1]/div/div[2]/input");
-		 txtAddName= ce("/html/body/div[1]/div[3]/form/div[2]/div[1]/div[1]/div/div[2]/input");
-		 txtAddSrNo= ce("/html/body/div[1]/div[3]/form/div[2]/div[1]/div[1]/div/div[2]/input");
-		 txtAddType= ce("/html/body/div[1]/div[3]/form/div[2]/div[1]/div[1]/div/div[2]/input");
-		 txtAddQuantity= ce("/html/body/div[1]/div[3]/form/div[2]/div[1]/div[1]/div/div[2]/input");
-
-	}
-		
-	@Test 
-	public void test01() throws IOException {
-		System.out.println("Running test 01");
 		String userType="";
 		String name="";
 		String empID="";
@@ -104,46 +72,54 @@ public class Submit {
 		HSSFSheet sheet=wb.getSheet("SignUp"); 
 		for (Row row:sheet) {
 			signUpLink.click();
-			 userType=row.getCell(0).getCellStyle().getDataFormatString();
-			 name=row.getCell(1).getCellStyle().getDataFormatString();
-			 empID=row.getCell(2).getCellStyle().getDataFormatString();
-			 gender=row.getCell(3).getCellStyle().getDataFormatString();
-			 location=row.getCell(4).getCellStyle().getDataFormatString();
-			 emialID=row.getCell(5).getCellStyle().getDataFormatString();
-			 if(userType.equalsIgnoreCase("Staff")) {
-				 Select selUserType= new Select(driver.findElement(By.id("ID of the user type dropdown")));
-				 selUserType.selectByVisibleText(userType);
-				 
-			 }
-			 txtName.sendKeys(name);
-			 txtEmpNo.sendKeys(empID);
-			 if(gender.equalsIgnoreCase("Male")) {
-				 radioGenderMale.click();
-				 
-			 }
-			 else {
-				 radioGenderFemale.click();
-			 }
-			 if(selLocation.getAttribute("disabled").equalsIgnoreCase("disabled")) {
-				 
-			 }
-			 else {
-				 Select selUserLocation= new Select(driver.findElement(By.id("ID of the user type dropdown")));
-				 selUserLocation.selectByVisibleText(location);
-			 }
-			 txtEmail.sendKeys(emialID);
-			 btnCreateUser.click();
-			 logger.info(lableSuccess.getText());
-			 
-			 
+			userType=row.getCell(0).getCellStyle().getDataFormatString();
+			name=row.getCell(1).getCellStyle().getDataFormatString();
+			empID=row.getCell(2).getCellStyle().getDataFormatString();
+			gender=row.getCell(3).getCellStyle().getDataFormatString();
+			location=row.getCell(4).getCellStyle().getDataFormatString();
+			emialID=row.getCell(5).getCellStyle().getDataFormatString();
+			if(userType.equalsIgnoreCase("Staff")) {
+				Select uType= new Select(driver.findElement(By.id("ID of the user type dropdown")));
+				uType.selectByVisibleText(userType);
+
+			}
+			txtName.sendKeys(name);
+			txtEmpNo.sendKeys(empID);
+			if(gender.equalsIgnoreCase("Male")) {
+				radioGenderMale.click();
+
+			}
+			else {
+				radioGenderFemale.click();
+			}
+			if(selLocation.getAttribute("disabled").equalsIgnoreCase("disabled")) {
+
+			}
+			else {
+				Select selUserLocation= new Select(driver.findElement(By.id("ID of the user type dropdown")));
+				selUserLocation.selectByVisibleText(location);
+			}
+			txtEmail.sendKeys(emialID);
+			btnCreateUser.click();
+			logger.info(lableSuccess.getText());
+
+
 		}
 
 	}
-	
+
 	@Test 
 	@Parameters({ "Type", "Name", "ServiceNo", "Quantity" })
 	public void test02(String type, String name, String srNo, String quantity) {
 		System.out.println(type+" "+name+" "+srNo+" "+quantity);
+
+		WebElement loginLink=ce("");
+		WebElement btnAddChem=ce("");
+		WebElement txtAddType=ce("");
+		WebElement txtAddName=ce("");
+		WebElement txtAddSrNo=ce("");
+		WebElement txtAddQuantity=ce("");
+		WebElement btnViewChem=ce("");
 		loginLink.click();
 		btnAddChem.click();
 		txtAddType.sendKeys(type);
@@ -151,7 +127,7 @@ public class Submit {
 		txtAddSrNo.sendKeys(srNo);
 		txtAddQuantity.sendKeys(quantity);
 		btnViewChem.click();
-		
+
 	}
 
 	public WebElement ce(String xpath) {
@@ -159,19 +135,48 @@ public class Submit {
 		ele=driver.findElement(By.xpath(xpath));
 		return ele;
 	}
+
+	public void switchWindow(String NewWinTitle) {
+		//String parent=driver.getWindowHandle();
+		Set<String>s=driver.getWindowHandles();
+		// Now iterate using Iterator
+		Iterator<String> I1= s.iterator();
+		while(I1.hasNext())
+		{
+			String child_window=I1.next();
+			driver.switchTo().window(child_window);
+			if(child_window.equalsIgnoreCase(NewWinTitle)) {
+				driver.switchTo().window(child_window);
+			}
+		}
+	}
+
+	public void closeWindows() {
+		Set<String>s=driver.getWindowHandles();
+		// Now iterate using Iterator
+		Iterator<String> I1= s.iterator();
+		while(I1.hasNext())
+		{
+			String child_window=I1.next();
+			driver.switchTo().window(child_window);
+			//System.out.println(driver.switchTo().window(child_window).getTitle());
+			driver.close();
+			driver.quit();
+		}
+	}
 	//release resources after test
 	@AfterMethod
 	public void release() {
-		driver.close();
-		driver.quit();
+		closeWindows();
+
 	}
 
 
-	
-	
 
 
 
-	
+
+
+
 
 }
